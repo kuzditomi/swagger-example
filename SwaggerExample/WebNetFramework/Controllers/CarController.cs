40d00b84e4de3e7b6bc04c5dba9d5199bfb4d4dd
Küzdi Tamás <kuzditomi@gmail.com>
2018-06-28 21:25:42 +0200

    add POST method

--- SwaggerExample/WebNetFramework/Controllers/CarController.cs
+++ SwaggerExample/WebNetFramework/Controllers/CarController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
 using WebNetFramework.Models;
 
 namespace WebNetFramework.Controllers
@@ -19,17 +20,70 @@ namespace WebNetFramework.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("")]
-        [SwaggerResponse(HttpStatusCode.OK, "All cars", typeof(Car))]
+        [SwaggerResponse(HttpStatusCode.OK, "All cars", typeof(Car[]))]
         public IHttpActionResult GetCars()
         {
             var cars = new List<Car>
             {
                 new Car {
-                   Id = 1
+                   Id = 1,
+                   PlateNumber= "ABC-123"
                 }
             };
 
             return Ok(cars);
+        }
+
+        /// <summary>
+        /// Returns car with given Id
+        /// </summary>
+        /// <param name="id">Unique id of car</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        [SwaggerResponse(HttpStatusCode.NotFound, "No car with given Id")]
+        [SwaggerResponse(HttpStatusCode.OK, "Returns car with given Id", typeof(Car))]
+        public IHttpActionResult GetCar(int id)
+        {
+            if (id != 1)
+            {
+                return NotFound();
+            }
+
+            return Ok(new Car
+            {
+                Id = 1,
+                PlateNumber = "ABC-123"
+            });
+        }
+
+        /// <summary>
+        /// Adds new car to system with given plate number
+        /// </summary>
+        /// <param name="car">Car creation data</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "The request is invalid.", typeof(ModelStateDictionary))]
+        [SwaggerResponse(HttpStatusCode.Conflict, "Car with given plate already exists")]
+        [SwaggerResponse(HttpStatusCode.OK, "Returns added car", typeof(Car))]
+        public IHttpActionResult AddCar(CarCreate car)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (car.PlateNumber == "ABC-123")
+            {
+                return Conflict();
+            }
+
+            return Ok(new Car
+            {
+                Id = 2,
+                PlateNumber = car.PlateNumber
+            });
         }
     }
 }
---
+++ SwaggerExample/WebNetFramework/Models/CarCreate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebNetFramework.Models
+{
+    /// <summary>
+    /// Used in car creation request
+    /// </summary>
+    public class CarCreate
+    {
+        /// <summary>
+        /// License plate number in format ABC-123
+        /// </summary>
+        [Required]
+        [RegularExpression("[A-Z]{3}-[0-9]{3}", ErrorMessage = "Plate number must be in format ABC-123")]
+        public string PlateNumber { get; set; }
+    }
+}
